a47967d3f3fc08ee48475bc02edb3844e1fc39f8
Panth Patel <panthhp@outlook.com>
2023-12-05 20:53:15 +0000

    2023 Day 5

--- src/main/kotlin/aoc2023/day5/Helper.kt
+++ src/main/kotlin/aoc2023/day5/Helper.kt
@@ -1,142 +1,25 @@
 package aoc2023.day5
 
 data class Almanac(
-	val seeds : List<Long>,
-	val seedToSoilMap : Map<Long, Long>,
-	val soilToFertilizerMap : Map<Long, Long>,
-	val fertilizerToWaterMap : Map<Long, Long>,
-	val waterToLightMap : Map<Long, Long>,
-	val lightToTemperatureMap : Map<Long, Long>,
-	val temperatureToHumidityMap : Map<Long, Long>,
-	val humidityToLocationMap : Map<Long, Long>,
+	val seeds: List<Long>,
+	val seedToSoilMap: Map<Long, Long>,
+	val soilToFertilizerMap: Map<Long, Long>,
+	val fertilizerToWaterMap: Map<Long, Long>,
+	val waterToLightMap: Map<Long, Long>,
+	val lightToTemperatureMap: Map<Long, Long>,
+	val temperatureToHumidityMap: Map<Long, Long>,
+	val humidityToLocationMap: Map<Long, Long>
 ) {
 	companion object {
-		fun fromLines(lines : List<String>) : Almanac {
-			val seeds = lines[0]
-				.substring(7)
-				.split(" ")
-				.map(String::trim)
-				.map(String::toLong)
-			val seedToSoilMap = mutableMapOf<Long, Long>()
-			val soilToFertilizerMap = mutableMapOf<Long, Long>()
-			val fertilizerToWaterMap = mutableMapOf<Long, Long>()
-			val waterToLightMap = mutableMapOf<Long, Long>()
-			val lightToTemperatureMap = mutableMapOf<Long, Long>()
-			val temperatureToHumidityMap = mutableMapOf<Long, Long>()
-			val humidityToLocationMap = mutableMapOf<Long, Long>()
-			
-			val entireFile = lines.joinToString("\n")
-			
-			for (
-				line in entireFile
-					.split("seed-to-soil map:")
-					.last()
-					.split("soil-to-fertilizer map:")
-					.first()
-					.split("\n")
-					.filterNot { it.isBlank() }
-			) {
-				val destStart = line.split(" ").first().trim().toLong()
-				val sourceStart = line.split(" ")[1].trim().toLong()
-				val rangeLength = line.split(" ").last().trim().toLong()
-				for (i in 0..<rangeLength) {
-					seedToSoilMap[sourceStart + i] = destStart + i
-				}
-			}
-			for (
-				line in entireFile
-					.split("soil-to-fertilizer map:")
-					.last()
-					.split("fertilizer-to-water map:")
-					.first()
-					.split("\n")
-					.filterNot { it.isBlank() }
-			) {
-				val destStart = line.split(" ").first().trim().toLong()
-				val sourceStart = line.split(" ")[1].trim().toLong()
-				val rangeLength = line.split(" ").last().trim().toLong()
-				for (i in 0..<rangeLength) {
-					soilToFertilizerMap[sourceStart + i] = destStart + i
-				}
-			}
-			for (
-				line in entireFile
-					.split("fertilizer-to-water map:")
-					.last()
-					.split("water-to-light map:")
-					.first()
-					.split("\n")
-					.filterNot { it.isBlank() }
-			) {
-				val destStart = line.split(" ").first().trim().toLong()
-				val sourceStart = line.split(" ")[1].trim().toLong()
-				val rangeLength = line.split(" ").last().trim().toLong()
-				for (i in 0..<rangeLength) {
-					fertilizerToWaterMap[sourceStart + i] = destStart + i
-				}
-			}
-			for (
-				line in entireFile
-					.split("water-to-light map:")
-					.last()
-					.split("light-to-temperature map:")
-					.first()
-					.split("\n")
-					.filterNot { it.isBlank() }
-			) {
-				val destStart = line.split(" ").first().trim().toLong()
-				val sourceStart = line.split(" ")[1].trim().toLong()
-				val rangeLength = line.split(" ").last().trim().toLong()
-				for (i in 0..<rangeLength) {
-					waterToLightMap[sourceStart + i] = destStart + i
-				}
-			}
-			for (
-				line in entireFile
-					.split("light-to-temperature map:")
-					.last()
-					.split("temperature-to-humidity map:")
-					.first()
-					.split("\n")
-					.filterNot { it.isBlank() }
-			) {
-				val destStart = line.split(" ").first().trim().toLong()
-				val sourceStart = line.split(" ")[1].trim().toLong()
-				val rangeLength = line.split(" ").last().trim().toLong()
-				for (i in 0..<rangeLength) {
-					lightToTemperatureMap[sourceStart + i] = destStart + i
-				}
-			}
-			for (
-				line in entireFile
-					.split("temperature-to-humidity map:")
-					.last()
-					.split("humidity-to-location map:")
-					.first()
-					.split("\n")
-					.filterNot { it.isBlank() }
-			) {
-				val destStart = line.split(" ").first().trim().toLong()
-				val sourceStart = line.split(" ")[1].trim().toLong()
-				val rangeLength = line.split(" ").last().trim().toLong()
-				for (i in 0..<rangeLength) {
-					temperatureToHumidityMap[sourceStart + i] = destStart + i
-				}
-			}
-			for (
-				line in entireFile
-					.split("humidity-to-location map:")
-					.last()
-					.split("\n")
-					.filterNot { it.isBlank() }
-			) {
-				val destStart = line.split(" ").first().trim().toLong()
-				val sourceStart = line.split(" ")[1].trim().toLong()
-				val rangeLength = line.split(" ").last().trim().toLong()
-				for (i in 0..<rangeLength) {
-					humidityToLocationMap[sourceStart + i] = destStart + i
-				}
-			}
+		fun fromLines(lines: List<String>): Almanac {
+			val seeds = lines[0].substring(7).split(" ").map(String::trim).map(String::toLong)
+			val seedToSoilMap = populateMap(lines, "seed-to-soil map:")
+			val soilToFertilizerMap = populateMap(lines, "soil-to-fertilizer map:")
+			val fertilizerToWaterMap = populateMap(lines, "fertilizer-to-water map:")
+			val waterToLightMap = populateMap(lines, "water-to-light map:")
+			val lightToTemperatureMap = populateMap(lines, "light-to-temperature map:")
+			val temperatureToHumidityMap = populateMap(lines, "temperature-to-humidity map:")
+			val humidityToLocationMap = populateMap(lines, "humidity-to-location map:")
 			
 			return Almanac(
 				seeds,
@@ -146,8 +29,29 @@ data class Almanac(
 				waterToLightMap,
 				lightToTemperatureMap,
 				temperatureToHumidityMap,
-				humidityToLocationMap,
+				humidityToLocationMap
 			)
+		}
+		
+		private fun populateMap(lines: List<String>, key: String): Map<Long, Long> {
+			val map = mutableMapOf<Long, Long>()
+			val keyLine = lines.indexOf(key)
+			val section = mutableListOf<String>()
+			var index = keyLine + 1
+			while (true) {
+				if (try { lines[index].isBlank() } catch (e: Throwable) { true }) break
+				section.add(lines[index])
+				index++
+			}
+			for (line in section.filterNot { it.isBlank() }) {
+				val destStart = line.split(" ").first().trim().toLong()
+				val sourceStart = line.split(" ")[1].trim().toLong()
+				val rangeLength = line.split(" ").last().trim().toLong()
+				(sourceStart..<sourceStart + rangeLength).forEach { i ->
+					map[i] = destStart + i - sourceStart
+				}
+			}
+			return map
 		}
 	}
 }
